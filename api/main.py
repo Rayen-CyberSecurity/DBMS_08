1969161956bca21cce8c427406c67091d687e44c
Rayen-CyberSecurity <rayenbhr31@gmail.com>
2026-07-08 00:54:23 +0200

    feat: move credentials to .env file

--- api/main.py
+++ api/main.py
@@ -1,14 +1,15 @@
 import psycopg2
 import psycopg2.extras
 from fastapi import FastAPI
+import os
 
 app = FastAPI(title="Studenten-API")
 
 DB_CONFIG = {
-    "dbname": "vorlesung",
-    "user": "vorlesung",
-    "password": "geheim",
-    "host": "postgres",   # container name as hostname
+    "dbname": os.environ.get("POSTGRES_DB", "vorlesung"),
+    "user": os.environ.get("POSTGRES_USER", "vorlesung"),
+    "password": os.environ.get("POSTGRES_PASSWORD", ""),
+    "host": "postgres",
     "port": 5432,
 }
 
